42aadc0e7c6c23f7c4e8ced575c63365d50bc936
juan diego <juadielonlop07gmail.com>
2026-03-03 08:52:41 -0500

    segundo commit inventario

--- Inventario.java
+++ Inventario.java
@@ -22,3 +22,55 @@ public class Inventario {
         listaProductos.add(nuevo);
         return true;
     }
+
+      public Producto buscarProducto(String codigo) {
+        for (Producto p : listaProductos) {
+            if (p.getCodigo().equalsIgnoreCase(codigo)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public boolean agregarUnidades(String codigo, int cantidad) {
+        Producto p = buscarProducto(codigo);
+        if (p != null) {
+            return p.aumentarStock(cantidad);
+        }
+        return false;
+    }
+
+    public boolean retirarUnidades(String codigo, int cantidad) {
+        Producto p = buscarProducto(codigo);
+        if (p != null) {
+            return p.disminuirStock(cantidad);
+        }
+        return false;
+    }
+
+    public void mostrarProductos() {
+        if (listaProductos.isEmpty()) {
+            System.out.println("No hay productos registrados.");
+        } else {
+            for (Producto p : listaProductos) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    public void mostrarBajoStock() {
+        for (Producto p : listaProductos) {
+            if (p.getCantidadStock() < 5) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    public double calcularValorTotalInventario() {
+        double total = 0;
+        for (Producto p : listaProductos) {
+            total += p.calcularValorTotal();
+        }
+        return total;
+    }
+}
