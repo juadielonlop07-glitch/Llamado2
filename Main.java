3d37866b0f31a8e8e5f84c16f53baac09485b48e
juan diego <juadielonlop07gmail.com>
2026-03-03 08:48:35 -0500

    segundo commit main

--- Main.java
+++ Main.java
@@ -19,3 +19,88 @@ public class Main {
             System.out.println("6. Calcular valor total del inventario");
             System.out.println("7. Salir");
             System.out.print("Seleccione una opción: ");
+             try {
+                opcion = sc.nextInt();
+                sc.nextLine();
+
+                switch (opcion) {
+
+                    case 1:
+                        System.out.print("Código: ");
+                        String codigo = sc.nextLine();
+
+                        System.out.print("Nombre: ");
+                        String nombre = sc.nextLine();
+
+                        System.out.print("Precio: ");
+                        double precio = sc.nextDouble();
+
+                        System.out.print("Stock inicial: ");
+                        int stock = sc.nextInt();
+
+                        if (inventario.registrarProducto(codigo, nombre, precio, stock)) {
+                            System.out.println("Producto registrado correctamente.");
+                        } else {
+                            System.out.println("Error al registrar producto.");
+                        }
+                        break;
+
+                    case 2:
+                        System.out.print("Código del producto: ");
+                        codigo = sc.nextLine();
+
+                        System.out.print("Cantidad a agregar: ");
+                        int cantidadAgregar = sc.nextInt();
+
+                        if (inventario.agregarUnidades(codigo, cantidadAgregar)) {
+                            System.out.println("Stock actualizado.");
+                        } else {
+                            System.out.println("No se pudo agregar unidades.");
+                        }
+                        break;
+
+                    case 3:
+                        System.out.print("Código del producto: ");
+                        codigo = sc.nextLine();
+
+                        System.out.print("Cantidad a retirar: ");
+                        int cantidadRetirar = sc.nextInt();
+
+                        if (inventario.retirarUnidades(codigo, cantidadRetirar)) {
+                            System.out.println("Venta realizada.");
+                        } else {
+                            System.out.println("No se pudo retirar unidades.");
+                        }
+                        break;
+
+                    case 4:
+                        inventario.mostrarProductos();
+                        break;
+
+                    case 5:
+                        inventario.mostrarBajoStock();
+                        break;
+
+                    case 6:
+                        System.out.println("Valor total inventario: $" +
+                                inventario.calcularValorTotalInventario());
+                        break;
+
+                    case 7:
+                        System.out.println("Saliendo del sistema...");
+                        break;
+
+                    default:
+                        System.out.println("Opción inválida.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debe ingresar el tipo de dato correcto.");
+                sc.nextLine();
+            }
+
+        } while (opcion != 7);
+
+        sc.close();
+    }
+}
