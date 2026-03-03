4b163133a364839fd4f12cba3b4c794140543529
juan diego <juadielonlop07gmail.com>
2026-03-03 08:55:40 -0500

    segundo commit producto

--- Producto.java
+++ Producto.java
@@ -31,3 +31,37 @@ public class Producto {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    public void setPrecioUnitario(double precioUnitario) {
+        if (precioUnitario >= 0) {
+            this.precioUnitario = precioUnitario;
+        }
+    }
+
+    public boolean aumentarStock(int cantidad) {
+        if (cantidad > 0) {
+            cantidadStock += cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean disminuirStock(int cantidad) {
+        if (cantidad > 0 && cantidad <= cantidadStock) {
+            cantidadStock -= cantidad;
+            return true;
+        }
+        return false;
+    }
+
+    public double calcularValorTotal() {
+        return precioUnitario * cantidadStock;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo +
+                " | Nombre: " + nombre +
+                " | Precio: $" + precioUnitario +
+                " | Stock: " + cantidadStock;
+    }
+}
